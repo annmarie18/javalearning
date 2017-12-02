1d46df9ddd1f246f8da70380129c7f6fc0e308a4
annmarie18 <annpdomini@gmail.com>
2017-12-02 13:02:13 +0530

    Packaging the existing project

---
+++ FilePreProcessing/com/amd/filepreprocessing/ExeMain.java
@@ -0,0 +1,11 @@
+package com.amd.filepreprocessing;
+import com.amd.filepreprocessing.*;
+
+public class ExeMain{  
+    public static void main(String args[]){  
+	  //Creating objects  
+	  FileChecker filechecker=new FileChecker();  
+		filechecker.fileChecker();
+	}  
+}
+
---
+++ FilePreProcessing/com/amd/filepreprocessing/FileChecker.java
@@ -0,0 +1,29 @@
+package com.amd.filepreprocessing;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FileChecker{ 
+	public void fileChecker(){
+		File f = new File("D:\\Prgms\\srcefiles");
+		boolean bool=false;
+		try{
+			bool = f.exists();
+			if (f.exists()){
+				// ArrayList<String> names = new ArrayList<String>(Arrays.asList(f.list()));  --to store just the file names
+				ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles())); // the full file path will be stored in the list
+				for (int i = 0; i < files.size(); i++) {
+					System.out.println(files.get(i));
+				}
+			}
+			else
+			{
+				System.out.println("File Exists : "+bool);
+			}
+		} catch (Exception e)
+		{
+		}
+	}  
+	
+}  
